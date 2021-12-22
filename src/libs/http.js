1ee1c08578e96a96d39c051b55eafba15ee431a1
Rebecca Mollica <rebecca.mollica@live.it>
2021-12-22 14:56:13 +0100

    updated code

--- src/libs/http.js
+++ src/libs/http.js
@@ -1,24 +1,36 @@
-const BASE_URL = 'https://edgemony-backend.herokuapp.com';
+const BASE_URL = "https://edgemony-backend.herokuapp.com";
 
-const http = (resource) => fetch(BASE_URL + resource)
-    .then(response => response.json());
+const http = (resource) =>
+  fetch(BASE_URL + resource).then((response) => response.json());
 
 const httpPost = (resource, data) =>
-    fetch(BASE_URL + resource, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    }).then(response => response.json());
+  fetch(BASE_URL + resource, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => response.json());
 
-    const httpDELETE = (resource) =>
-    fetch(BASE_URL + resource, {
-        method: 'DELETE',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify(),
-    }).then(response => response.json());
+const httpDELETE = (resource) =>
+  fetch(BASE_URL + resource, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((response) => response.json());
 
-export { http, httpPost, httpDELETE };
+// Popola la lista degli amici, in base alla lista friendsNames
+const fillFriendsList = (friendsNames) => {
+  let value = 0;
+  for (value in [...Array(friendsNames.length).keys()]) {
+    httpPost("/friends", {
+      name: friendsNames[value],
+      photo: `https://randomuser.me/api/portraits/men/${value}.jpg`,
+    });
+
+    setTimeout(() => {}, 100);
+  }
+};
+
+export { http, httpPost, httpDELETE, fillFriendsList };
--- src/pages/Friends/index.jsx
+++ src/pages/Friends/index.jsx
@@ -1,22 +1,33 @@
-import { useEffect, useState } from 'react';
-import { http } from '../../libs/http';
-import FriendPreview from '../../components/FriendPreview';
-import styles from './Friends.module.scss';
+import { useState, useEffect } from "react";
+import CreateFriend from "../../components/CreateFriend";
+import FriendPreview from "../../components/FriendPreview";
+// import Pagination from "./../../components/Pagination";
+import { http } from "../../libs/http";
+import styles from "./Friends.module.scss";
 
 const Friends = () => {
-    const [friendsList, setFriendsList] = useState([]);
+  const [friendsList, setFriendsList] = useState([]);
+  // const [pagination, setPagination] = useState(1);
 
-    useEffect(() => {
-        http('/friends').then((friendsList) => setFriendsList(friendsList))
-    }, []);
+  useEffect(() => {
+    http(`/friends?_page=1`).then((friendsList) => setFriendsList(friendsList));
+  }, []);
 
-    return (
-        <div className={styles.friends}>
-            {friendsList.map((friend) => (
-                <FriendPreview data={friend} key={friend.id} />
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.Friends}>
+      <CreateFriend />
+
+      {/* <div>
+        <Pagination />
+      </div> */}
+
+      <div className={styles.__friendsList}>
+        {friendsList.map((friend) => (
+          <FriendPreview data={friend} key={friend.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Friends;
