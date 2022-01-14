f22e363a9e76c9aa867ff9f0f72d50b16ec45e90
Rebecca Mollica <rebecca.mollica@live.it>
2022-01-14 09:09:12 +0100

    aggiunta spiegazione history location e navigator

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -6,6 +6,11 @@ import Header from './Header';
 test('render and check the <Header /> component', () => {
     const history = createMemoryHistory();
 
+    // l'oggetto history deve essere splittato in due modi,
+    // perché la location non viene più presa in maniera implicita
+    // ma va passata a parte, così da avere un router implementato
+    // lato node che abbia tutto il necessario per partire
+
     render(
         <Router location={history} navigator={history}>
             <Header />
@@ -47,5 +52,17 @@ test('render and check the <Header/> component with custom with custom links', (
     const lis = container.querySelectorAll('nav li');
     expect(lis.length).toBe(lis.length);
 });
+
+// normalmente, l'oggetto history esiste già nel browser.
+// lato node, però, questo oggetto non esiste e la funzione
+// => const history = createMemoryHistory(); 
+// ha il compito di crearla da zero con de valori predefiniti.
+// viene poi quindi passato a router per consentirgli di 
+// appoggiarsi a tale oggetto e non rompersi.
+//* è un mezzo mock: 
+// da una parte non lo è perché non si stanno usando
+// funzionalità di jest, dall'altra lo è perché in pratica 
+// si sta creando una nuova entità con la struttura dell'history  
+// ma senza valori reali
 
 // l'oggetto history è una virtualizzazione del router
