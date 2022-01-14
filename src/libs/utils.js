bb0613c19e797b30226997a02ea33dc7d4731fc4
Rebecca Mollica <rebecca.mollica@live.it>
2022-01-14 23:10:15 +0100

    feat: added fetch test reference

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -2,4 +2,8 @@
 
 const greetings = (name) => `Hello dear ${name}`;
 
-export { greetings };
+const doFetch = () =>
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.json());
+
+export { greetings, doFetch };
